72328bc37042af569c934b48bffcb8c20797fb56
akommi <archu.85@gmail.com>
2017-05-17 14:01:21 -0500

    Script for testing mutiple sites

--- public/js/archanajs.js
+++ public/js/archanajs.js
@@ -1,4 +1,9 @@
-function myFunction() {
- alert('this is TAG');
-
+function myFunction(customers) {
+ 
+ var customerArray = customers.split(" ");
+ console.log(customerArray);
+ for(var i=0; i<customerArray.length;i++){
+   var customer = customerArray[i].trim();
+   window.open('http://' + customer + '.clickmotivefusionstage.com');
+ }
 }
