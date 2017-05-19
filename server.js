ca18ba549480c3b59a5875d2d6c3a9776d5267e8
Kyle Crouse <kylec296@gmail.com>
2017-05-18 23:12:37 -0500

    Fixing the issue with js that is not node modules and cleaning up inline js

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = __dirname + '/views/';
 
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get('/', function(req, res){
-res.sendFile(path + '/index.html');
+	res.sendFile(path + '/index.html');
+});
+
+
+app.get("/archanaPage",function(req,res){
+    res.sendFile(path + "archanaPage.html");
+});
+
+app.get("/IgorPage", function (req, res) {
+    res.sendFile(path + "IgorPage.html");
 });
 
 app.get('/kylesPage', function(req, res){
